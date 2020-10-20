4135a3096e8e5cc0d1e03be371b920d06c71c878
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-10-20 15:50:48 -0700

    A lot of stuff I should've commited earlier

---
+++ Game Subfolder/Assets/LevelLoader.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    private bool playerDetected;
+    public Transform doorPos;
+    public float width;
+    public float height;
+
+    public LayerMask whatIsPlayer;
+
+    private int currentLevel;
+
+    public void Start()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        playerDetected = Physics2D.OverlapBox(doorPos.position, new Vector2(width, height), 0, whatIsPlayer);
+        
+        if (playerDetected == true)
+        {
+            if (Input.GetButtonDown("Up"))
+            {
+                LoadNextLevel();
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(doorPos.position, new Vector3(width, height, 1));
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(currentLevel + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
---
+++ Game Subfolder/Assets/leverTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leverTrigger : MonoBehaviour
+{
+    public Sprite LeverOn;
+    public GameObject other;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (other.gameObject.GetComponent<SpriteRenderer>().sprite == LeverOn)
+        {
+            Destroy(gameObject);
+            Destroy(this);
+            Destroy(GetComponent<Rigidbody>());
+        }
+    }
+}
