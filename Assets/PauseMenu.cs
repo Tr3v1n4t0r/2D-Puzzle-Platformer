a9b0346adcf99e237f2cabf7191d36a10e0befcb
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-11-04 17:17:31 -0800

    Added comments

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -5,15 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
+    public static bool GameIsPaused = false;    // Boolean used to designate whether or not the game is paused
 
-    public GameObject pauseMenuUI;
+    public GameObject pauseMenuUI;  // Public object used to display the wanted pause UI
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Escape"))
         {
+            // Pauses or unpauses the game when the Escape button is pushed
             if (GameIsPaused)
             {
                 Resume();
@@ -27,26 +28,36 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume()
     {
+        // Turns off the pause screen
         pauseMenuUI.SetActive(false);
+        // Sets the timescale to normal speed
         Time.timeScale = 1f;
+        // Sets boolean to false
         GameIsPaused = false;
     }
 
     void Pause()
     {
+        // Turns on the pause screen
         pauseMenuUI.SetActive(true);
+        // Sets the timescale to 0 so game objects are unable to move in the background
         Time.timeScale = 0f;
+        // Sets boolean to true
         GameIsPaused = true;
     }
 
+    // Funcions that are accessed by in game buttons
+    
     public void LoadMenu()
     {
+        // Sets timescale to normal speed
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
 
     public void QuitGame()
     {
+        // Debug.Log used when testing in game
         Debug.Log("Quitting game...");
         Application.Quit();
     }
