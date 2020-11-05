3d2fd06f7bb6305fe2221585ad3075737bc0dccc
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-11-04 17:34:26 -0800

    Added comments

--- Assets/leverTrigger.cs
+++ Assets/leverTrigger.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class leverTrigger : MonoBehaviour
 {
-    public Sprite LeverOn;
-    public GameObject other;
+    public Sprite LeverOn;    // Public attribute used to "see" a specific sprite
+    public GameObject other;    // Public attribute used to view the lever game object
 
     // Update is called once per frame
     void Update()
     {
+        // Checks to see if the lever game object is in the "on" position
         if (other.gameObject.GetComponent<SpriteRenderer>().sprite == LeverOn)
         {
+            // When the lever is "on", the gate that this object that this function is attached to is deleted
             Destroy(gameObject);
+            // "this" refers to this file (might be unnecessary)
             Destroy(this);
+            // The "Rigidbody" refers to the hitbox of the current object (might be unnecessary)
             Destroy(GetComponent<Rigidbody>());
         }
     }
