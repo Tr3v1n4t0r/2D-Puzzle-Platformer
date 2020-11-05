ad1ecff18d2b3befdf74d93d721ee55350676556
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-11-04 17:06:49 -0800

    Added comments

--- Assets/Lever.cs
+++ Assets/Lever.cs
@@ -4,28 +4,31 @@ using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
-    public Sprite LeverOff;
-    public Sprite LeverOn;
+    public Sprite LeverOff;    // Public attribute used to get an in game sprite
+    public Sprite LeverOn;    // Public attribute used to get an in game sprite
 
-    private bool playerDetected;
-    private bool diskDetected;
-    public Transform switchPos;
-    public float width;
-    public float height;
+    private bool playerDetected;    // Bool that is used to detect when a player's hitbox is overlapping a specified location
+    private bool diskDetected;    // Bool that is used to detect when a disk's hitbox is overlapping a specified location
+    public Transform switchPos;    // A game object that dictates where the center of a square is drawn from
+    public float width;    // Signifies the width of the square mentioned above
+    public float height;    // Signifies the height of the square mentioned above
 
-    public LayerMask whatIsPlayer;
-    public LayerMask whatIsDisk;
+    public LayerMask whatIsPlayer;    // Public mask used to define what the player is
+    public LayerMask whatIsDisk;    // Public mask used to define what the disk is
 
     // Update is called once per frame
     private void Update()
     {
+        // Sets the bool to true if the "whatIsPlayer" mask overlaps the box drawn in the function OnDrawGizmosSelected
         playerDetected = Physics2D.OverlapBox(switchPos.position, new Vector2(width, height), 0, whatIsPlayer);
+        // Sets the bool to true if the "whatIsDisk" mask overlaps the box drawn in the function OnDrawGizmosSelected
         diskDetected = Physics2D.OverlapBox(switchPos.position, new Vector2(width, height), 0, whatIsDisk);
 
         if (playerDetected == true)
         {
             if (Input.GetButtonDown("Up"))
             {
+                // Changes the lever sprite depending on which state it's currently in
                 if (this.gameObject.GetComponent<SpriteRenderer>().sprite == LeverOn)
                 {
                     this.gameObject.GetComponent<SpriteRenderer>().sprite = LeverOff;
@@ -39,13 +42,17 @@ public class Lever : MonoBehaviour
 
         if (diskDetected == true)
         {
+            // Turns the lever on if the disk overlaps it
             this.gameObject.GetComponent<SpriteRenderer>().sprite = LeverOn;
         }
     }
 
+    // Function used to draw a wire box
     private void OnDrawGizmosSelected()
     {
+        // Colors the box blue
         Gizmos.color = Color.blue;
+        // Draws a Cube based on an in game object and a given width and height (depth doesn't matter since it's a 2D game)
         Gizmos.DrawWireCube(switchPos.position, new Vector3(width, height, 1));
     }
 }
